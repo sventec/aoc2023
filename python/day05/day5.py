32c14035e6b2931bf1648a5b8f121915dbb562b4
sventec <fx@fewrx.net>
2023-12-05 15:24:22 -0500

    finish day 5 part 2

--- python/day05/day5.py
+++ python/day05/day5.py
@@ -42,6 +42,7 @@ with open("input.txt", "r", encoding="utf8") as f:
 grps = lines.split("\n\n")
 seeds = [int(seed) for seed in grps.pop(0).split()[1:]]
 
+
 def get_locs(seeds):
     # locs = {s: s for s in seeds}
     l_locs = list(seeds)
@@ -49,7 +50,6 @@ def get_locs(seeds):
         new_locs = {}
         maps = grp.splitlines()[1:]
         # valid_maps = [m for m in maps if set(seeds) & set(range(int((s := m.split())[2])))]
-        # grp_map = {}
         for m in maps:
             dest, src, l = (int(n) for n in m.split())
             rng = range(src, src + l)  # + 1?
@@ -64,19 +64,38 @@ def get_locs(seeds):
                 l_locs[i] = new_locs[loc]
     return l_locs
 
-        # if set(seeds) & set(range(int(src), int(src) + int(l))):
-        #     grp_map = grp_map | {int(src) + i: int(dest) + i for i in range(int(l))}
-    # for l_src, l_dest in locs.items():
-    #     locs[l_src] = grp_map.get(l_dest) or locs[l_src]
 
-# part_1 = min(locs, key=locs.get)
-
-# print(f"Part 1: {min(locs.values())}")
 print(f"Part 1: {min(get_locs(seeds))}")
 
-# BUG: Just runs out of time/memory, modify loc checker function to accept range to check without needing to create entire range in list
-seeds_range = []
-for i in range(0, len(seeds), 2):
-    seeds_range.extend(range(seeds[i], seeds[i] + seeds[i + 1]))
 
-print(f"Part 2: {min(get_locs(seeds_range))}")
+def part_2(grps, seeds):
+    seed_ranges = [(s[0], s[1]) for s in zip(seeds[::2], seeds[1::2], strict=True)]
+
+    loc_grps = grps[-1]
+    max_loc = max([int(m.split()[0]) for m in loc_grps.splitlines()[1:]])
+
+    try:
+        from tqdm import tqdm
+
+        iter_range = tqdm(range(max_loc))
+    except ImportError:
+        iter_range = range(max_loc)
+
+    for i in iter_range:
+        i_map = int(i)
+        for grp in grps[::-1]:
+            for m in grp.splitlines()[1:]:
+                dest, src, l = (int(n) for n in m.split())
+                rng = range(dest, dest + l)
+                if i_map in rng:
+                    # print(f"map: {m}")
+                    # print(f"{i} in range: {rng}")
+                    i_map = src + rng.index(i_map)
+                    # print(f"new: {i_map}\n")
+                    break
+        if any(i_map in range(s[0], s[0] + s[1]) for s in seed_ranges):
+            return i
+    return -1
+
+
+print(f"Part 2: {part_2(grps, seeds)}")
