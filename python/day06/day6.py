f47a5a6b59bca30b14375a63a1e13c5f7a9dd4bc
sventec <fx@fewrx.net>
2023-12-06 01:44:27 -0500

    use quadratic for part 1 also

--- python/day06/day6.py
+++ python/day06/day6.py
@@ -6,26 +6,34 @@ with open("input.txt", "r", encoding="utf8") as f:
     lines = f.read().splitlines()
 
 lt, ld = ([int(i) for i in lines[n].split()[1:]] for n in range(2))
-
-# races are tuples of (time, distance)
 races = [(t, d) for t, d in zip(lt, ld)]  # noqa: B905
-r_times = [(len([t for t in range(r[0]) if (r[0] - t) * t > r[1]])) for r in races]
 
+# Brute force solve method (works fine)
+# r_times = [(len([t for t in range(r[0]) if (r[0] - t) * t > r[1]])) for r in races]
+# print(f"Part 1: {math.prod(r_times)}")
+
+# Quadratic formula, get values at each X (time) intercept -- min & max of winning time range
+# Formula for intercepts, transformed from line 12 above. x is t:
+# -x^2 + time * x - distance = 0
+def q(b, c):
+    # 'a' of formula is always -1, has been simplified out
+    res = []
+    d = b * b + 4 * c
+    s = math.sqrt(abs(d))
+    for op in (-1, 1):
+        root = (-b + s * op) / -2
+        if int(root) == root:
+            # Perfect square (tie), offset 1
+            root += op
+        res.append(root)
+    # [winning time max, winning time min]
+    return math.floor(res[0]) - math.ceil(res[1]) + 1
+
+r_times = [q(t, d * -1) for t, d in races]
 print(f"Part 1: {math.prod(r_times)}")
 
 time = int("".join(str(n) for n in lt))
 distance = int("".join(str(d) for d in ld))
 
-# Quadratic formula, get values at each X (time) intercept -- min & max of winning time range
-# Formula for intercepts, transformed from line 12 above. x is t:
-# -x^2 + time * x - distance = 0
-def q(a, b, c):
-    res = []
-    d = b * b - 4 * a * c
-    s = math.sqrt(abs(d))
-    for op in (-1, 1):
-        res.append((-b + s * op) / (2 * a))
-    return res
-
-ts = q(-1, time, distance * -1)  # [winning time max, winning time min]
-print(f"Part 2: {round(ts[0] - ts[1])}")
+ts = q(time, distance * -1)
+print(f"Part 2: {ts}")
